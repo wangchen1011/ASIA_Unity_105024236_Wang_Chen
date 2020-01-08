277349967f6da350963c8de2ebbae5c06ff72927
wangchen1011 <55476741+wangchen1011@users.noreply.github.com>
2020-01-08 18:14:35 +0800

    第二版

---
+++ Unity_105024236_chen/Assets/g.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class g : MonoBehaviour
+{
+
+    public Transform cam, target;
+    public int speed = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * Time.deltaTime * speed);
+        cam.position = pos;
+    }
+}
---
+++ Unity_105024236_chen/Assets/monster.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+
+    [Header("移動速度")]
+    [Range(1, 2000)]
+    public int speed = 10;
+    [Header("旋轉速度"), Tooltip("精靈旋轉速度"), Range(1.5f, 200f)]
+    public float turn = 20.5f;
+    [Header("撿東西位置")]
+    public Rigidbody rigCatch;
+
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+
+    void Update()
+    {
+        Turn();
+        Run();
+        Attack();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(other.name);
+        if (other.name == "rock" && ani.GetCurrentAnimatorStateInfo(0).IsName("accatk"))
+        {
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            other.GetComponent<HingeJoint>().connectedBody = rigCatch;
+        }
+    }
+
+    private void Turn()
+    {
+        float h = Input.GetAxis("Horizontal");
+        tran.Rotate(0, turn * h * Time.deltaTime, 0);
+    }
+    private void Run()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("attack")) return;
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
+        ani.SetBool("walk", v != 0);
+    }
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("atteck");
+        }
+    }
+}
